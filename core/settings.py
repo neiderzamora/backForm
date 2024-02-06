65d388e58c6f805a45216f25cbd2576fa9d91663
ihuntersouls <neiderzamoramoreno@gmail.com>
2024-02-06 12:13:05 -0500

    version 1.2

--- core/settings.py
+++ core/settings.py
@@ -108,5 +108,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
