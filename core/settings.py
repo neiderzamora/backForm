5da822a8846398aef6edaa61acab1bddb948f409
ihuntersouls <neiderzamoramoreno@gmail.com>
2024-02-06 11:53:26 -0500

    delete code static files

--- core/settings.py
+++ core/settings.py
@@ -100,13 +100,5 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
