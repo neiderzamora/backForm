ce2923c9df6c4fd15f0242a4d632f3dc5b52a35e
ihuntersouls <neiderzamoramoreno@gmail.com>
2024-02-06 12:03:28 -0500

    version 1.0

--- core/settings.py
+++ core/settings.py
@@ -100,5 +100,13 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
