9275d983b91bbf16f275d03a5a22c6cd2563d038
ihuntersouls <neiderzamoramoreno@gmail.com>
2024-02-08 11:16:41 -0500

    add email config enviroment

--- core/settings.py
+++ core/settings.py
@@ -84,12 +84,12 @@ DATABASES = {
 # Configuration email
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'mail.hygmicropigmentacion.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'contacto@hygmicropigmentacion.com'
-EMAIL_HOST_PASSWORD = 'Claves2024*'
-DEFAULT_FROM_EMAIL = 'contacto@hygmicropigmentacion.com'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', default=465))
+EMAIL_USE_SSL = bool(os.environ.get('EMAIL_USE_SSL', default=True))
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 AUTH_PASSWORD_VALIDATORS = [
     {
